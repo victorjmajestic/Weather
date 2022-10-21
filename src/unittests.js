48e29849599e7521fc5301ed6a7e78e9a266bcb6
victorjmajestic <vjm38@case.edu>
2022-10-21 14:37:25 -0400

    added chai/mocha unit test functionality
    only files that were changed were index.html, and added unittests.js

---
+++ src/unittests.js
@@ -0,0 +1,24 @@
+const chai = window.chai
+//const expect = chai.expect
+//const assert = chai.assert
+//const should = chai.should()
+//UNCOMMENT EACH IF IT IS REQUIRED FOR ANY TESTS
+
+//For info about what to write for each test visit https://www.chaijs.com/guide/styles/
+//Tests will appear at the bottom of index.html, comment tests out for the final product
+
+/* FORMAT FOR UNIT TESTS
+describe('methodName', () => {
+    it('insert method function here' () => {
+        *test function here
+    })
+})
+*/
+
+/* EXAMPLE UNIT TEST
+describe('sumOf', () => {
+    it('computes the sum of two integers' () => {
+        expect(sumOf(15, 11)).to.equal(26)
+    })
+})
+*/
--- weather++/src/index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("Service Worker registered properly");
-        console.log(registration);
-    }).catch(error => {
-        console.log("Service Worker failed to register");
-        console.log(error);
-    })
-}
